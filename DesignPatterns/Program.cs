d097a4905442ac9f425e1f14c91f82f8f8024923
htrellez <htrellez@icloud.com>
2026-02-10 14:46:42 -0600

    memento

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,10 +1,24 @@
-﻿// Dependency Inversion Principle (DIP):
+﻿// Memento
 
-/*
- * High-level modules should not depend on low-level modules. Both should depend on
- * abstraction
- */
+// The Memento Pattern is used to restore an object to previous state.
 
-using DesignPatterns.SOLID.D;
+using DesignPatterns.DesignPatterns.Behavioral.Memento;
 
-var car = new Car(new Engine());
+var editor = new Editor();
+var history = new History(editor);
+
+history.Backup();
+editor.Title = "Test";
+history.Backup();
+editor.Content = "Hello there, my name is Lalo";
+history.Backup();
+editor.Title = "The life of dev: my mementos";
+
+Console.WriteLine("Title: " + editor.Title);
+Console.WriteLine("Content: " + editor.Content);
+
+history.Undo();
+Console.WriteLine("Title: " + editor.Title);
+Console.WriteLine("Content: " + editor.Content);
+
+history.ShowHistory();
